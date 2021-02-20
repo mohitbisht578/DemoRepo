cb32b3b2322fa008667ebfd04d8664f6953004f1
Mohit Bisht <mohitbisht@Mohits-MacBook-Air.local>
2021-02-20 20:14:15 +0530

    added deals page code

---
+++ src/main/java/com/qa/pages/DealsPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+import org.testng.reporters.jq.Main;
+
+public class DealsPage {
+	
+	@Test
+	public void dealsPage() {
+		
+		System.out.println("This is deals Page");
+	}
+
+}
