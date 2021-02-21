b3d54676b8a1964e73f90fb787537afde0924fe5
Mohit Bisht <mohitbisht@Mohits-MacBook-Air.local>
2021-02-21 17:06:24 +0530

    Architect development

---
+++ src/main/java/com/qa/util/AppiumTest.java
@@ -0,0 +1,17 @@
+package com.qa.util;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSAPPS");
+	}
+	
+	@Test
+	public void nativeAppAndriods() {
+		System.out.println("ANDROID APP");
+	}
+
+}
--- src/main/java/com/qa/util/TestUtil.java
+++ src/main/java/com/qa/util/TestUtil.java
@@ -1,8 +1,16 @@
 package com.qa.util;
 
+import org.testng.annotations.Test;
+
 public class TestUtil {
 
+	@Test
 	public void testUtil() {
 		System.out.println("test util");
+	}
+	
+	@Test
+	public void restApi() {
+		System.out.println("REST API");
 	}
 }
